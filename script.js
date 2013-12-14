2af6770b6dbfee430f90e7fead460f7158dc4010
Karina Ramos <kramos@cristoreyny.org>
2013-12-14 15:12:30 -0500

    adding avenue code

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var trs = 100;
 var tds = 100;
+function isAvenue(col)
+{
+    return col % 3 == 0 
+}
 
 document.write("<table id='map' border='1px'>");
 for (var i =0 ; i < trs; i++) {
     document.write("<tr>");
     for (var x =0 ; x < tds; x++) {
-        document.write("<td></td>");
-    }
+        if (isAvenue(x)) {
+            document.write("<td class=\"avenue\"></td>");
+        } else {
+            document.write("<td></td>");
+        }
+    } 
     document.write("</tr>");
 }
 document.write("</table>");

d77a9f6f13f4576f4521afdde0fdd74db08e3782
Ben Sussman <benjamin.sussman@gmail.com>
2013-12-14 16:46:19 -0500

    now you can only go on the grid

--- script.js
+++ script.js
@@ -23,38 +23,48 @@ function isAvenue(col) {
     return avenues[col]; 
 }
 
-var car1 = [4,4];
+var carX = 0;
+var carY = 0;
 var changeColor = function(x,y){
-	var selector = 'tr:nth-child('+Math.floor(y).toString()+') td:nth-child('+Math.floor(x).toString()+')';
-	console.log(typeof selector);
+    x++;
+    y++;
+    console.log("x:"+ x + " y:" + y)
+	var selector = 'tr:nth-child('+y.toString()+') td:nth-child('+x.toString()+')';
 	$(selector).css('background', 'red');
 	// $('tr:nth-child(50) td:nth-child(49)').css('background', 'yellow');
 }; 
+$(function(){
+    changeColor(carX,carY);
+});
 $(document).on('keypress', function(event){
 	var keyCode = event.keyCode;
 			up = 119,
 			right = 100,
 			left = 97,
-			down = 115;
+			down = 115,
+			newCarX = carX,
+			newCarY = carY;
 
 			if (keyCode == up){
 				console.log('up');
-				car1[1] = car1[1] - 3;
-				console.log(car1);
+				newCarY = carY - 1;
 			} else if (keyCode == right){
 				console.log('right');
-				car1[0] = car1[0] + 3;
-				console.log(car1);
+				newCarX = carX + 1;
 			} else if (keyCode == left){
 				console.log('left');
-				car1[0] = car1[0] - 3;
-				console.log(car1);
+				newCarX = carX - 1;
 			} else if (keyCode == down){
 				console.log('down');
-				car1[1] = car1[1] + 3;
-				console.log(car1);
+				newCarY = carY + 1;
 			}
-			changeColor(car1[0]/3,car1[1]/3);
+			
+			if (isAvenue(newCarX) || isStreet(newCarY)) {
+			    carY = newCarY;
+			    carX = newCarX;
+			}
+			
+			changeColor(carX,carY);
 			
 });
 

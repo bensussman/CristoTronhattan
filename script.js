d7935e8fd8d60226cb8d345e534c37d4e7b104f1
chubyaca <jcuevas@cristoreyny.org>
2013-12-14 13:37:26 -0500

    adding javascript File for table creation

---
+++ script.js
@@ -0,0 +1,12 @@
+var trs = 34;
+var tds = 12;
+
+document.write("<table id='map'>");
+for (var i =0 ; i < trs; i++) {
+	document.write("<tr>");
+	for (var x =0 ; x < tds; x++) {
+	document.write("<td>"+x+"</td>");
+	}
+	document.write("</tr>");
+}
+document.write("</table>");

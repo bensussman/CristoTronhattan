d3af86a1d47473a2f6c40aa79fbb4117244f25bd
skud <st4sik@gmail.com>
2013-12-14 16:23:48 -0500

    add movement

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var car1 = [200,200];
-
+var car1 = [100,100];
+var changeColor = function(x,y){
+	var selector = 'tr:nth-child('+Math.floor(y).toString()+') td:nth-child('+Math.floor(x).toString()+')';
+	console.log(typeof selector);
+	$(selector).css('background', 'red');
+	// $('tr:nth-child(50) td:nth-child(49)').css('background', 'yellow');
+}; 
 $(document).on('keypress', function(event){
 	var keyCode = event.keyCode;
 			up = 119,
@@ -11,6 +16,7 @@ $(document).on('keypress', function(event){
 				console.log('up');
 				car1[1] = car1[1] - 3;
 				console.log(car1);
+				changeColor(car1[0]/3,car1[1]/3);
 			} else if (keyCode == right){
 				console.log('right');
 				car1[0] = car1[0] + 3;
